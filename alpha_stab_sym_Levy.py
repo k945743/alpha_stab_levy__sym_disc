56a4c1c86fa9412d5f98f3a413d58097e02f77eb
Lóránt Nagy <lorantnagy@Lorants-MacBook-Pro.local>
2024-01-29 12:39:48 +0100

    major revision of code

--- alpha_stab_sym_Levy.py
+++ alpha_stab_sym_Levy.py
@@ -7,42 +7,12 @@ import numpy as np
 # seq_len = len(G) = len(W)
 # alpha \in (0,2]
 
-# core method:
-""" def alpha_stab_levy(times, alpha, G,W ):
-    inc=[0]
-    N=len(times)
-    for i in range(N-1):
-        inc.append((times[i+1]-times[i])**(1/alpha)*(np.sin(alpha*G[i]))/((np.cos(G[i]))**(1/alpha))*((np.cos((1-alpha)*G[i]))/(W[i]))**((1-alpha)/(alpha)))
-    return np.cumsum(inc) """
-
-#EXAMPLE:  direct usage of the methods "alpha_stab_levy"
-""" time_zero = 0
-horizon = 1
-seq_len = 100
-times=[i*horizon/seq_len for i in range(time_zero,seq_len)]
-low=-np.pi/2
-high=np.pi/2
-G=np.random.uniform(low,high,seq_len)
-W=np.random.exponential(1,seq_len)
-alpha = 1.1
-path=alpha_stab_levy(times,alpha,G,W)
-#plt.plot(path) """
-####                              ####
-
 
 class AlphaStabSymLevy():
     
-    def __init__(self,XXX,alpha,mode="seq_len"):
-        if mode == "seq_len":
-            self.seq_len = XXX[0]
-            self.time_horizon = XXX[1]
-            self.times = [i*self.time_horizon/self.seq_len for i in range(self.seq_len)]
-        elif mode == "times":
-            self.times = XXX
-            self.seq_len = len(self.times)-1
-        else:
-            print("ERROR")
-            
+    def __init__(self, times, alpha):
+        self.times = times
+        self.seq_len = len(self.times)-1 
         self.alpha = alpha
     
     def alpha_stab_levy(self, times, alpha, G,W ):
@@ -62,17 +32,12 @@ class AlphaStabSymLevy():
 
 #EXAMPLES:
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 #EXAMPLE 1:
-
-#levy = AlphaStabSymLevy([1000,1],1.3)
-#plt.plot(levy())
-
-#EXAMPLE 2:
-
-#horizon = 1
-#seq_len = 1000
-#times=[i*horizon/seq_len for i in range(seq_len)]
-#levy_t = AlphaStabSymLevy(times,1.3,switch="times")
-#plt.plot(levy_t())
+horizon = 1
+seq_len = 1000
+times=[i*horizon/seq_len for i in range(seq_len)]
+levy = AlphaStabSymLevy(times,1.3)
+plt.plot(levy())
+plt.show()
